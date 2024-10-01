521e39474221c0a98c8d644292a364577d01a1b8
Morteza Asadi <morteza.asadi@live.com>
2024-10-01 14:55:09 +0200

    refactor: simplify equality check logic for enum string comparer

--- src/FluentChangeDetector/Comparers/EnumStringPropertyComparer.cs
+++ src/FluentChangeDetector/Comparers/EnumStringPropertyComparer.cs
@@ -6,11 +6,9 @@ public class EnumStringPropertyComparer<TEnum> : IPropertyComparer where TEnum :
 {
     public new bool Equals(object? x, object? y)
     {
-        if (x is not TEnum xEnum || y is not TEnum yEnum) return false;
-        
-        var xString = xEnum.ToString();
-        var yString = yEnum.ToString();
-            
+        var xString = x?.ToString();
+        var yString = y?.ToString();
+
         return string.Equals(xString, yString, StringComparison.OrdinalIgnoreCase);
-    } 
+    }
 }
